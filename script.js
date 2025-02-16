d2589a3b7457ec4e5d181a791e2368bf268e863e
De1t4s <71740812+de1t4s@users.noreply.github.com>
2025-02-16 20:32:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -143,3 +143,31 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+        const popup = document.getElementById("whitelist-popup");
+
+        if (popup) {
+            popup.style.display = "flex";
+            setTimeout(() => {
+                popup.classList.add("active");
+            }, 200);
+
+            setTimeout(() => {
+                popup.classList.remove("active");
+                setTimeout(() => {
+                    popup.style.display = "none";
+                }, 300);
+            }, 5000);
+        }
+    }, 100);
+});
+
+
+
+
+
+
+
+
+
